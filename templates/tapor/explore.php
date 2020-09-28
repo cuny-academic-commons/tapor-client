61b4d7777ec93a5d199929bdadd494d509166c3e
Boone B Gorges <boonebgorges@gmail.com>
2020-09-28 13:41:21 -0500

    Dropdown selection for Category on Explore template.

--- templates/tapor/explore.php
+++ templates/tapor/explore.php
@@ -2,7 +2,8 @@
 <h3><?php esc_html_e( 'Explore', 'tapor-client' ) ?></h3>
 
 <?php
-$url = remove_query_arg( array( 'tapor-category', 'tapor-search' ), bp_get_requested_url() );
+$url          = remove_query_arg( array( 'tapor-category', 'tapor-search' ), bp_get_requested_url() );
+$selected_cat = isset( $_GET['tapor-category'] ) ? $_GET['tapor-category'] : null;
 ?>
 
 <form method="get" action="<?php echo esc_attr( $url ) ?>#tapor-results">
@@ -14,7 +15,7 @@ $url = remove_query_arg( array( 'tapor-category', 'tapor-search' ), bp_get_reque
 		<select name="tapor-category" id="tapor-category">
 			<option value=""></option>
 			<?php foreach ( $categories as $cat ) : ?>
-				<option value="<?php echo intval( $cat['tid'] ) ?>"><?php echo esc_html( $cat['name'] ) ?></option>
+				<option value="<?php echo intval( $cat['tid'] ) ?>" <?php selected( $cat['tid'], $selected_cat ); ?>><?php echo esc_html( $cat['name'] ) ?></option>
 			<?php endforeach ?>
 		</select>
 		<input class="dirt-explore-button" type="submit" value="<?php _e( 'Go', 'dirt-directory-client' ) ?>" />
